859e32343c322f9ca28316c60e0f538544fd35e5
incremania <106455212+incremania@users.noreply.github.com>
2024-02-22 13:57:57 +0100

    Update app.ts

--- src/configs/app.ts
+++ src/configs/app.ts
@@ -29,7 +29,7 @@ const initializeMiddlewares = () => {
     "http://127.0.0.1:3000",
     "http://localhost:3001",
     "http://127.0.0.1:3001",
-    "https://sprinterstechnologies.com"
+    "https://sprinterstechnologies.com",
     "https://sprinterz.netlify.app",
   ];
 
